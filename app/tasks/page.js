a770beedfa7665fb3415713d724d2123f6b96f19
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2024-01-20 23:27:19 +0100

    included create tasks and delete tasks functionality

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,16 @@
+import TaskForm from "@/components/TaskForm";
+import TaskList from "@/components/TaskList";
 import React from "react";
 
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <>
+      <div className="max-w-lg">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </>
+  );
 };
 
 export default Tasks;
